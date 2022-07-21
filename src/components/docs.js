4addbcb97c928fa96ef80a90a7ddd68b7c1d24f6
Monday_David_Sarka <david_md@hamari.agency>
2022-07-21 15:01:28 +0100

    Installed material UI and added modal

--- src/components/docs.js
+++ src/components/docs.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Modal from './modal';
 
 
 export default function Docs() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    
     return(
         <div className="docs-main">
             <h1>Daveed!</h1>
-            <button className="add-docs">
+            <button className="add-docs" onClick={handleOpen}>
                 Add a Document
             </button>
+
+            <Modal
+            open={open}
+            setOpen={setOpen}
+            />
         </div>
 
     )
---
+++ src/components/modal.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50% , -50%)',
+    width: '400',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
+
+export default function ModalComponent({
+    open,
+    setOpen,
+}) {
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Modal
+                open={open}
+                onclose={handleClose}
+                arial-labelledby="modal-modal-title"
+                arial-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{mt: 2}}>
+                        Douis Molis Et Unnam, Sanct Text in a modal. Commodo
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
