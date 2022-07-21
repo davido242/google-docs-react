f29cf236affb168ce5de4850a378a26866034089
Monday_David_Sarka <david_md@hamari.agency>
2022-07-21 13:49:10 +0100

    added font-family from google font cdn

---
+++ src/components/docs.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Docs() {
+    return(
+        <div className="docs-main">
+            <h1>Daveed!</h1>
+            <button className="add-docs">
+                Add a Document
+            </button>
+        </div>
+
+    )
+}
